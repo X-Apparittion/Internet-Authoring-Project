c2e1ed563f0f090ff51e5316afa7555356864c90
X-Apparittion <61167644+X-Apparittion@users.noreply.github.com>
2020-04-17 21:46:46 -0500

    Changes to database and insert of javascript api

--- CAPPA/resources/views/CrimePostIndex.blade.php
+++ CAPPA/resources/views/CrimePostIndex.blade.php
@@ -10,9 +10,109 @@
                     <div>
                         <div class="uk-card uk-card-default uk-card-large uk-card-body box2">
                             <h3 class="uk-card-title">Map of crime</h3>
-                            <p>
-                                MAP API
-                            </p>
+                            <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+                            <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+                            
+                            <style>
+                                #map {
+                                    height: 100%;
+                                }
+                                /* Optional: Makes the sample page fill the window. */
+                                html, body {
+                                    height: 100%;
+                                    margin: 0;
+                                    padding: 0;
+                                }
+                            </style>
+
+                            <body>
+                                <div id="map"></div>
+
+                                <script>
+                                var customLabel = {
+                                    restaurant: {
+                                    label: 'R'
+                                    },
+                                    bar: {
+                                    label: 'B'
+                                    }
+                                    House: {
+                                    label: 'H'
+                                    }
+                                    Street: {
+                                    label: 'S'
+                                    }
+                                    Car: {
+                                    label: 'C'
+                                    }
+
+                                };
+
+                                function initMap() {
+                                    var map = new google.maps.Map(document.getElementById('map'), {
+                                    center: new google.maps.LatLng(18.014115, -77.501873),
+                                    zoom: 12
+                                    });
+                                    var infoWindow = new google.maps.InfoWindow;
+
+                                    // Change this depending on the name of your PHP or XML file
+                                    downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
+                                        var xml = data.responseXML;
+                                        var markers = xml.documentElement.getElementsByTagName('marker');
+                                        Array.prototype.forEach.call(markers, function(markerElem) {
+                                        var id = markerElem.getAttribute('id');
+                                        var name = markerElem.getAttribute('name');
+                                        var address = markerElem.getAttribute('address');
+                                        var type = markerElem.getAttribute('location type');
+                                        var point = new google.maps.LatLng(
+                                            parseFloat(markerElem.getAttribute('lat')),
+                                            parseFloat(markerElem.getAttribute('lng')));
+
+                                        var infowincontent = document.createElement('div');
+                                        var strong = document.createElement('strong');
+                                        strong.textContent = name
+                                        infowincontent.appendChild(strong);
+                                        infowincontent.appendChild(document.createElement('br'));
+
+                                        var text = document.createElement('text');
+                                        text.textContent = address
+                                        infowincontent.appendChild(text);
+                                        var icon = customLabel[type] || {};
+                                        var marker = new google.maps.Marker({
+                                            map: map,
+                                            position: point,
+                                            label: icon.label
+                                        });
+                                        marker.addListener('click', function() {
+                                            infoWindow.setContent(infowincontent);
+                                            infoWindow.open(map, marker);
+                                        });
+                                        });
+                                    });
+                                    }
+
+                                    function downloadUrl(url, callback) {
+                                        var request = window.ActiveXObject ?
+                                            new ActiveXObject('Microsoft.XMLHTTP') :
+                                            new XMLHttpRequest;
+
+                                        request.onreadystatechange = function() {
+                                        if (request.readyState == 4) {
+                                            request.onreadystatechange = doNothing;
+                                            callback(request, request.status);
+                                        }
+                                        };
+
+                                        request.open('GET', url, true);
+                                        request.send(null);
+                                    }
+
+                                    function doNothing() {}
+                                    </script>
+                                    <script async defer
+                                    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+                                    </script>
+                                </body>
                         </div>
                     </div>
                 </div>
